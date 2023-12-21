f13068ae39f25b5acc08899a91affd3972853309
niels.soete <niels.soete@student.howest.be>
2023-12-21 14:17:35 +0100

    chore: add hints to the login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,8 +5,16 @@ export default function Login() {
         <section className="login">
             <h2>Log in</h2>
             <form action="#">
-                <input type="text" placeholder="username" />
-                <input type="password" placeholder="password" />
+                <input
+                    type="text"
+                    placeholder="username"
+                    data-hint="The username is not that hard. Its the most used username for administrators."
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    data-hint="Remember what took over the world in the Terminator? The answer lies in that file."
+                />
                 <button type="submit">Log in</button>
             </form>
         </section>
