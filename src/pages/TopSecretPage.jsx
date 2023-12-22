5d990613d4ffaa2101946e1a04b5d253816f8638
niels.soete <niels.soete@student.howest.be>
2023-12-22 16:24:44 +0100

    chore: create the topsecret page

--- src/pages/TopSecretPage.jsx
+++ src/pages/TopSecretPage.jsx
@@ -1,3 +1,26 @@
+import React from "react";
+
 export default function TopSecretPage() {
-    return <div>TopSecretPage</div>;
+    const handleCopyToClipboard = () => {
+        const encryptedVersion = "8087a52e0e37d2babef10aaf1c40ffc5";
+        navigator.clipboard.writeText(encryptedVersion);
+    };
+
+    return (
+        <>
+            <p>
+                Hi Bart, this page is in case you forgot your password. Im not
+                going to give you the password, but here is the encrypted
+                version:{" "}
+                <span
+                    className="accent bold"
+                    onClick={handleCopyToClipboard}
+                    style={{ cursor: "pointer" }}
+                >
+                    8087a52e0e37d2babef10aaf1c40ffc5
+                </span>{" "}
+                p.s. remember your dogs name...
+            </p>
+        </>
+    );
 }
