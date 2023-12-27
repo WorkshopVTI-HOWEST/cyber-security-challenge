5c4fc430f761d8ff42944f1ccc8057ea361e1aa8
niels.soete <niels.soete@student.howest.be>
2023-12-27 13:57:46 +0100

    chore: add localstorage item when the component is mounted

--- src/pages/KeepThisHiddenPage.jsx
+++ src/pages/KeepThisHiddenPage.jsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+
 export default function KeepThisHiddenPage() {
+    useEffect(() => {
+        localStorage.setItem(
+            "secret URL",
+            "Someone once told me that there is a URL. Not a single person ever found it because it's 'topsecret'."
+        );
+    }, []);
     return (
         <>
             There is nothing on this page. Maybe there is something stored on
