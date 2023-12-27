655cd329d7ff3d5b4012c76eb115b5dc78d1f40c
niels.soete <niels.soete@student.howest.be>
2023-12-27 13:50:25 +0100

    chore: create keep this hidden page

---
+++ src/pages/KeepThisHiddenPage.jsx
@@ -0,0 +1,8 @@
+export default function KeepThisHiddenPage() {
+    return (
+        <>
+            There is nothing on this page. Maybe there is something stored on
+            your machine?
+        </>
+    );
+}
